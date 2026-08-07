0c1102ef868a4d92308badf7429d9dbcd45a8a35
leejia0324 <hx.un0707@gmail.com>
2026-08-08 08:19:53 +0900

    feat: temp auth pages

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
+import 'package:moly_mobile/features/auth/presentation/pages/sign_in_page.dart';
+import 'package:moly_mobile/features/auth/presentation/pages/sign_up_page.dart';
 
 final GoRouter appRouter = GoRouter(
   initialLocation: '/',
   routes: [
     GoRoute(path: '/', builder: (context, state) => const Placeholder()),
+    GoRoute(path: '/signIn', builder: (_, _) => const SignInPage()),
+    GoRoute(path: '/signUp', builder: (_, _) => const SignUpPage()),
   ],
 );
