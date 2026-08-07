92294d0cb53b69fc9b3d8ef3637fa8d58bf166b8
leejia0324 <hx.un0707@gmail.com>
2026-08-08 06:49:37 +0900

    feat: add go_router

---
+++ lib/core/router/app_router.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+import 'package:go_router/go_router.dart';
+
+final GoRouter appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(path: '/', builder: (context, state) => const Placeholder()),
+  ],
+);
