99b760feafebae679acee4b6301da702f9effff7
leejia0324 <hx.un0707@gmail.com>
2026-08-08 07:25:32 +0900

    feat: add svg assets file

---
+++ lib/core/constants/svg_assets.dart
@@ -0,0 +1,8 @@
+class SvgAssets {
+  static const expedition = 'assets/svg/expedition_icon.svg';
+  static const map = 'assets/svg/map_icon.svg';
+  static const moleSelected = 'assets/svg/mole_icon_selected.svg';
+  static const moleUnselected = 'assets/svg/mole_icon_unselected.svg';
+  static const logo = 'assets/svg/moly_logo.svg';
+  static const profile = 'assets/svg/profile_icon.svg';
+}
